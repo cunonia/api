08a664f15abd5c1be4440ef6207b0e98960939d9
Dhinak G <dhinak2004@gmail.com>
2020-12-26 22:27:18 -0500

    update

--- cog.py
+++ cog.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 
 class BotCog(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.settings = self.bot.settings(self)
         super().__init__()
