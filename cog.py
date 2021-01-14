987a7e1b375a4a44b034385f6e7d9bddb0aaa98d
Dhinak G <dhinak2004@gmail.com>
2021-01-13 19:50:15 -0500

    Add typing

--- cog.py
+++ cog.py
@@ -4,5 +4,5 @@ from discord.ext import commands
 class BotCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.settings = self.bot.settings(self)
+        self.settings: dict = self.bot.settings(self)
         super().__init__()
